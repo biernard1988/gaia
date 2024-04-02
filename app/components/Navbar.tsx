164b2d977bdb6ac91409a4c1cf46bd1bccb6c6c4
Bernard Almeida da Costa <biernard@gmail.com>
2024-04-02 22:47:44 +0200

    finished the navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,22 +1,32 @@
 import Link from "next/link";
-import NavWallpaper from "./NavWallpaper";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <div>
-      <NavWallpaper />
-      <nav className="flex justify-between p-3 mx-auto border border-green-700 container">
-        <div>
-          <span className="text-xl font-extrabold border border-red-700">
-            GAIA
-          </span>
-        </div>
-        <div className="flex justify-evenly border border-red-700 w-1/2">
+    <nav className="relative h-125">
+      <div className="absolute inset-0 z-[1]">
+        <Image
+          src="/nav-bg.png"
+          alt="Foto da Chacara Panoramica"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        />
+        <div className="absolute bg-black opacity-40 inset-0"></div>
+      </div>
+      <div className="relative container mx-auto flex flex-col justify-center items-center space-y-8 text-white px-4 z-10">
+        <div className="flex justify-evenly w-1/2 mt-5 text-xl">
           <Link href={""}>Home</Link>
           <Link href={""}>About</Link>
           <Link href={""}>Contacts</Link>
         </div>
-      </nav>
-    </div>
+        <Image
+          src="/logo.png"
+          alt="Logotipo da Chacara"
+          width={200}
+          height={200}
+        />
+      </div>
+    </nav>
   );
 }
