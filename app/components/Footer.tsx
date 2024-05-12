188d5dd0a19d5c97e8d2d84caa3859164e1d82e7
Bernard Almeida da Costa <biernard@gmail.com>
2024-05-12 02:17:15 +0200

    fixed media queries in the footer section

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ import { TbBrandAirbnb } from "react-icons/tb";
 
 export default function Footer() {
   return (
-    <section className="h-56 place-content-center px-10 lg:px-20 text-white border-t border-neutral-800">
+    <section className="h-56 place-content-center px-8 lg:px-20 text-white border-t border-neutral-800">
       <div className="flex justify-between mt-10">
         <div className="flex flex-col w-1/3 gap-5">
           <Link className="hover:text-amber-300" href="#home">
